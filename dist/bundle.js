f2d6ec7dcc03e1278645318ec28a1b67e117a6e1
Adam Gray <adam@pateo.co>
2017-10-04 10:20:35 +0100

    Change default example

--- dist/bundle.js
+++ dist/bundle.js
@@ -90,10 +90,9 @@ const image = new Image();
 image.onload = function () {
   canvas.context.drawImage(this, 0, 0);
   let imageData = canvas.context.getImageData(0, 0, width, height);
-  filter.convolution(imageData, filter.emboss);
-  // filter.grayscale(imageData)
-  // filter.sepia(imageData)
-  // filter.convolution(imageData, filter.gaussian)
+  filter.grayscale(imageData);
+  filter.sepia(imageData);
+  filter.convolution(imageData, filter.gaussian);
   canvas.context.putImageData(imageData, 0, 0);
 };
 image.src = 'rose.jpg';
